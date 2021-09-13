403d64291d4d77fad8f59ca713684fbbc75a03c1
Cesar-Morales <moraleescesar@gmail.com>
2021-09-13 12:50:43 -0300

    recibiendo datos

--- semana1/ACMContestScoring.py
+++ semana1/ACMContestScoring.py
@@ -1,1 +1,15 @@
 # ACM Contest Scoring = https://vjudge.net/problem/Kattis-acm
+
+from sys import stdin
+  
+# Salvo todo en un arreglo de listas
+numsArr = []  
+aux = stdin.readline()  
+while (aux[:-1] != "-1"):
+    numsArr.append(aux[:-1].split(" "))
+    aux = stdin.readline()
+
+# Guardo datos particulares en diccionarios
+
+for n in numsArr:
+  print (n[0])
