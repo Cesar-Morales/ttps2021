64ce0adfbf15c555c19b9e52eec027ac3a6f21b8
Cesar-Morales <moraleescesar@gmail.com>
2021-09-13 14:19:20 -0300

    lo resuelve solo para uno

--- semana1/ACMContestScoring.py
+++ semana1/ACMContestScoring.py
@@ -3,13 +3,27 @@
 from sys import stdin
   
 # Salvo todo en un arreglo de listas
-numsArr = []  
-aux = stdin.readline()  
-while (aux[:-1] != "-1"):
-    numsArr.append(aux[:-1].split(" "))
-    aux = stdin.readline()
+diccionarioDeDatos = dict()  
+ultimoLeido = stdin.readline()  
+while (ultimoLeido[:-1] != "-1"):
+    ultimosDatosLista = ultimoLeido[:-1].split(" ")
+    if(ultimosDatosLista[1] in diccionarioDeDatos.keys()):
+        if("right" not in diccionarioDeDatos[ultimosDatosLista[1]]): 
+            diccionarioDeDatos[ultimosDatosLista[1]]= diccionarioDeDatos[ultimosDatosLista[1]] + [ultimosDatosLista[0],ultimosDatosLista[2]]
+    else:
+        diccionarioDeDatos[ultimosDatosLista[1]]=[ultimosDatosLista[0],ultimosDatosLista[2]]
+    ultimoLeido = stdin.readline()
 
 # Guardo datos particulares en diccionarios
+print(diccionarioDeDatos.keys())
+print(diccionarioDeDatos.values())
+print(diccionarioDeDatos["B"][0::2])
+lista = diccionarioDeDatos["B"][0::2]
 
-for n in numsArr:
-  print (n[0])
+# Probando una suma
+tot = (len(lista)-1)*20
+for index, num in enumerate(lista):
+    tot+=int(num)
+print(tot)
+
+
