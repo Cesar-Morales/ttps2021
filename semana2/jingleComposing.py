51d0d257d7597e88a1ab66a187c23d1a9f4bb2a8
Cesar-Morales <moraleescesar@gmail.com>
2021-09-17 19:07:32 -0300

    jingleComposing funciona pero da wrong (revisar). Estructura de semana 2 completa

--- semana2/jingleComposing.py
+++ semana2/jingleComposing.py
@@ -7,12 +7,22 @@ from sys import stdin
 
 notes = {"W":1,"H":1/2,"Q":1/4,"E":1/8,"S":1/16,"T":1/32,"X":1/64}
 
+
 def addNumbers(measure):
     tot = 0
     for note in measure:
-        tot += int(notes[note])
+        tot += float(notes[note])
     return tot
 
+def returnAmount(arr):
+    amount = 0
+    for measure in arr[1:-1]:
+        if (addNumbers(measure) == 1):
+            amount+=1
+    return amount
+    
 strArr = [str(x) for x in stdin.readline().split("/")]
-for measure in strArr[1:-1]:
-    print(addNumbers(measure))
+try:
+    print(returnAmount(strArr))
+except:
+    print("")
