3bd1b4a7e40ca540e15e6a29150611b5c62dd5d8
Cesar-Morales <moraleescesar@gmail.com>
2021-09-24 19:36:14 -0300

    DataStructure OK

--- semana2/iCanTheDataStructure.py
+++ semana2/iCanTheDataStructure.py
@@ -1,52 +1,55 @@
 # https://vjudge.net/problem/UVA-11995
 
-from sys import stdin, stdout
+from sys import stdin
 from collections import deque
 from heapq import *
 
+def check(is_stack, is_queue, is_priotiry_queue):
+  if not is_stack and not is_priotiry_queue and not is_queue:
+    print("impossible")
+  elif is_stack and not is_priotiry_queue and not is_queue:
+    print("stack")
+  elif is_priotiry_queue and not is_stack and not is_queue:
+    print("priority queue")
+  elif is_queue and not is_stack and not is_priotiry_queue:
+    print("queue")
+  else:
+    print("not sure")
+
+def main():
+  stack = [] 
+  queue = deque() 
+  priority_queue = [] 
+  is_stack = is_queue = is_priotiry_queue = True
+  cant_elements = 0
+  
+  n = int(input())
+  for i in range(n):
+    query = list(map(int, stdin.readline().strip().split()))
+    if query[0] == 1:
+      if is_stack:
+        stack.append(query[1])
+      if is_queue:
+        queue.appendleft(query[1])
+      if is_priotiry_queue:
+        heappush(priority_queue, -query[1])
+      cant_elements += 1
+    elif query[0] == 2:
+      if cant_elements == 0:
+        is_priotiry_queue = is_queue = is_stack = False
+      else:
+        if is_priotiry_queue and heappop(priority_queue) != -query[1]:
+          is_priotiry_queue = False
+        if is_queue and queue.pop() != query[1]:
+          is_queue = False
+        if is_stack and stack.pop() != query[1]:
+          is_stack = False
+      cant_elements -= 1
+  check(is_stack, is_queue, is_priotiry_queue)
+
+
 while True:
   try:
-    
-    
-
-    n_stack = []  # stack
-    n_queue = deque()
-    n_heap = []
-
-    is_stack = is_queue = is_heap = True
-    num_elements = 0
-    for i in range(n):
-      query = list(map(int, stdin.readline().strip().split()))
-      if query[0] == 1:
-        if is_stack:
-          n_stack.append(query[1])
-        if is_queue:
-          n_queue.appendleft(query[1])
-        if is_heap:
-          heappush(n_heap, -query[1])
-        num_elements += 1
-      elif query[0] == 2:
-        if num_elements == 0:
-          is_heap = is_queue = is_stack = False
-        else:
-          if is_heap and heappop(n_heap) != -query[1]:
-            is_heap = False
-          if is_queue and n_queue.pop() != query[1]:
-            is_queue = False
-          if is_stack and n_stack.pop() != query[1]:
-            is_stack = False
-        num_elements -= 1
-      
-    if not is_stack and not is_heap and not is_queue:
-      print("impossible")
-    elif is_stack and not is_heap and not is_queue:
-      print("stack")
-    elif is_heap and not is_stack and not is_queue:
-      print("priority queue")
-    elif is_queue and not is_stack and not is_heap:
-      print("queue")
-    else:
-      print("not sure")
-
+    main()
   except(EOFError):
     break
