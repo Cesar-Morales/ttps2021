931b878cf03520a1c16dcfd2e40605ee2620bb54
Cesar-Morales <moraleescesar@gmail.com>
2021-09-28 16:31:05 -0300

    babelfish accepted

--- semana2/babelfish.py
+++ semana2/babelfish.py
@@ -1,30 +1,37 @@
 # https://vjudge.net/problem/UVA-10282
+# Accepted
 
 from sys import stdin
 
 
-def load_dictionary():
-    pass
+def load_dictionary(arr_num):
+    dict_of_words[arr_num[1]] = arr_num[0]
 
 
-def read_input():
-    pass
+def read_input(num):
+    if(num in dict_of_words.keys()):
+        print(dict_of_words[num])
+    else:
+        print("eh")
 
 
 def main():
     load_dict = True
     for number in stdin:
         num = number.strip()
-        if (num == "0 0"):
-            break
         if (num == ""):
             load_dict = False
-        if (load_dict):
-            # cargo el diccionario
-            load_dictionary()
         else:
-            # leo entrada e imprimo resultado
-            read_input()
+            if (load_dict):
+                # cargo el diccionario
+                load_dictionary(num.split(" "))
+            else:
+                # leo entrada e imprimo resultado
+                read_input(num)
 
 
-main()
+dict_of_words = dict()
+try:
+    main()
+except:
+    pass
--- semana2/cd.py
+++ semana2/cd.py
@@ -4,6 +4,22 @@ from sys import stdin
 while True:
     try:
         number = stdin.readline().strip().split(' ')
-        print (number)
+        print(number)
     except:
-        break; 
+        break
+"""
+load_dict = True
+    for number in stdin:
+        num = number.strip()
+        if (num == "fin"):
+            break
+        if (num == ""):
+            load_dict = False
+        else:
+            if (load_dict):
+                # cargo el diccionario
+                load_dictionary(num.split(" "))
+            else:
+                # leo entrada e imprimo resultado
+                read_input(num)
+"""
