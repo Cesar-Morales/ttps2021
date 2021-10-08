366465ab7e9586a0c4078abfd887a1a622da0530
Cesar-Morales <moraleescesar@gmail.com>
2021-10-07 23:01:06 -0300

    arreglar debug matriz vacations extend

--- semana3 | lcs y lis /LCS/probando_matrices.py
+++ semana3 | lcs y lis /LCS/probando_matrices.py
@@ -1,13 +1,32 @@
-print("Ingrese filas: ")
-n = int(input())
-print("Ingrese columnas: ")
-m = int(input())
+
+
+def crear_matriz(n, m):
+    for i in range(n):
+        matriz.append([-1]*m)
+
+
+def imprimir_matriz(n, m):
+    print("\t", "x", end=" ")
+    for i in range(m):
+        print("\t", "c{}".format(i), end=" ")
+    print()
+    for f in range(n):
+        for c in range(m):
+            if c == 0:
+                print("\t", "f{}".format(f), end=" ")
+            print("\t", matriz[f][c], end=" ")
+        print()
+
+
+def main():
+    print("Ingrese filas: ")
+    n = int(input())
+    print("Ingrese columnas: ")
+    m = int(input())
+    crear_matriz(n, m)
+    imprimir_matriz(n, m)
+
 
 matriz = []
-for i in range(n):
-    matriz.append([]*m)
 
-#matriz[0][0] = 1111
-#matriz[n-1][m-1] = 9999
-
-print(matriz)
+main()
--- semana3 | lcs y lis /LCS/vacations_enxtend.py
+++ semana3 | lcs y lis /LCS/vacations_enxtend.py
@@ -3,29 +3,49 @@
 memo = []
 
 
+def imprimir_matriz(n, m, s, t):
+    print("\t", "-----DEBUG MATRIZ-----")
+    print("\t", "x", end=" ")
+    for i in range(m):
+        if(i == 0):
+            print("\t", "NULL", end=" ")
+        else:
+            print("\t", "{}".format(t[i-1]), end=" ")
+    print()
+    for i in range(n):
+        for j in range(m):
+            if j == 0 and i == 0:
+                print("\t", "NULL", end=" ")
+            elif j == 0:
+                print("\t", "{}".format(s[i-1]), end=" ")
+            print("\t", memo[i][j], end=" ")
+        print()
+    print()
+
+
 def LCS(s, t):
     n = len(s) + 1
     m = len(t) + 1
     for i in range(n):
-        memo.append([-9999]*m)
+        memo.append([-1]*m)
 
     for i in range(n):
         memo[i][0] = 0
     for j in range(m):
         memo[0][j] = 0
 
-    for f in range(n):
-        for c in range(m):
-            print("\t", memo[f][c], end=" ")
-        print()
-    """for i in n:
-        for j in m:
+    #imprimir_matriz(s, t)
+
+    for i in range(n):
+        for j in range(m):
             if s[i-1] == t[j-1]:
                 memo[i][j] = memo[i-1][j-1] + 1
             else:
                 memo[i][j] = max(memo[i-1][j], memo[i][j-1])
-    return memo[n-1][m-1]"""
+    imprimir_matriz(n, m, s, t)
+    return memo[n-1][m-1]
 
 
-#   columna, fila
-LCS("HOLA", "chauchau")
+set_of_cities = input()
+set_of_cities2 = input()
+print(LCS(set_of_cities, set_of_cities2))
