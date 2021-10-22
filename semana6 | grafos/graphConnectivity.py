ca890ab32f19cdcc2001b6279b2643ad18a88c6f
Cesar-Morales <moraleescesar@gmail.com>
2021-10-21 21:51:01 -0300

    bfs en python y notas de grafos

--- semana6 | grafos/graphConnectivity.py
+++ semana6 | grafos/graphConnectivity.py
@@ -1,1 +1,14 @@
 # https://vjudge.net/problem/UVA-459
+
+import queue
+
+def bfs(adj, ini, visit):
+    q = queue.Queue()
+    q.put(ini)
+    while not q.empty():
+        current = q.get()
+        visit[current]=1
+        for child in adj[current]:
+            if not visit[child]:
+                q.put(child)
+
